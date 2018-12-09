c4308a1dd29495fbb001ac103549c043a5bbb3bb
wqzwh <wangziqihappy@163.com>
2018-12-09 12:08:31 +0800

    feature: taro最新版升级，分类页面左侧菜单切换事件添加

--- src/pages/cate/index.js
+++ src/pages/cate/index.js
@@ -14,37 +14,37 @@ export default class Cate extends Component {
         label: '12.12专区',
         selected: true
       },{
-        value: 1,
+        value: 2,
         label: '冬季专区'
       },{
-        value: 1,
+        value: 3,
         label: '爆品专区'
       },{
-        value: 1,
+        value: 4,
         label: '新品专区'
       },{
-        value: 1,
+        value: 5,
         label: '居家'
       },{
-        value: 1,
+        value: 6,
         label: '鞋包服饰'
       },{
-        value: 1,
+        value: 7,
         label: '服装'
       },{
-        value: 1,
+        value: 8,
         label: '电器'
       },{
-        value: 1,
+        value: 9,
         label: '服装'
       },{
-        value: 1,
+        value: 10,
         label: '洗护'
       },{
-        value: 1,
+        value: 11,
         label: '饮食'
       },{
-        value: 1,
+        value: 12,
         label: '餐厨'
       }],
       banner: `${ASSETSURL}/img/cate.jpg`,
@@ -60,9 +60,27 @@ export default class Cate extends Component {
       }]
     }
   }
+
+  selected(item, e) {
+    const { leftNav } = this.state
+    for(let v of leftNav) {
+      if(v.value === item.value) {
+        v.selected = true
+      } else {
+        v.selected = false
+      }
+    }
+    this.setState({
+      leftNav: this.state.leftNav
+    })
+  }
+
   render () {
     const leftNavItem = this.leftNav.map((item) => {
-      return <View className={item.selected ? 'cate-left-item selected' : 'cate-left-item'} key={item.value}>{item.label}</View>
+      return <View 
+                className={item.selected ? 'cate-left-item selected' : 'cate-left-item'} 
+                key={item.value}
+                onClick={this.selected.bind(this, item)}>{item.label}</View>
     })
     const cateListItem = this.cateList.map((item) => {
       return <View className='item' key={item.value}>
